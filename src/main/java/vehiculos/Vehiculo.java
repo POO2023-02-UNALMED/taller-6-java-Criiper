3575fb33aa8c9873fcf45ef0f92a64b247a869ae
Criiper <perezloperacristiandavid@gmail.com>
2023-10-03 12:26:54 -0500

    Commit :beer:
    constructor correcioon

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -13,6 +13,18 @@ public class Vehiculo {
 	protected Fabricante fabricante;
 	protected static int cantidadVehiculos;
 	protected static ArrayList<Vehiculo> vehiculos = new ArrayList<>();
+	
+	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante) {
+		this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+		
+	}
 
 	public String vehiculoPorTipo() {
 		return "Automoviles: " + Automovil.getCantidadAutomoviles() 
